5d5fbf50d3dfd414984bf6d8fe4c800a9d9eeca1
Keanred <47241653+Keanred@users.noreply.github.com>
2026-06-10 18:36:11 +0300

    Initial Commit

---
+++ SnipKit/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  SnipKit
+//
+//  Created by Anssi Keinänen on 10.6.2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ SnipKitTests/SnipKitTests.swift
@@ -0,0 +1,19 @@
+//
+//  SnipKitTests.swift
+//  SnipKitTests
+//
+//  Created by Anssi Keinänen on 10.6.2026.
+//
+
+import Testing
+@testable import SnipKit
+
+struct SnipKitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
